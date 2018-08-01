7661fe52e798baf846844c044b135a999fd726fa
linuxandchill <rivercodi@gmail.com>
2018-07-31 19:59:56 -0700

    readFile

---
+++ Employee.h
@@ -0,0 +1,13 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H 
+
+struct Employee{
+  int ID;
+  std::string name;
+  double salary;
+  double pf;
+  double insurance;
+  std::string email; 
+}; 
+
+#endif
--- main.cc
+++ main.cc
@@ -3,26 +3,28 @@
 #include <fstream>
 #include <sstream> 
 #include <cstdlib> 
+#include "Employee.h"
 
 using namespace std;
 
-struct Employee{
-  int ID;
-  std::string name;
-  double salary;
-  double pf;
-  double insurance;
-  std::string email; 
-}; 
+void readFile(std::vector<Employee>& employee_list);
 
-void readFile(std::vector<Employee> employee_list);
-
-void readFile(std::vector<Employee> employee_list){
+void readFile(std::vector<Employee>& employee_list){
   std::ifstream fin("MOCK_DATA.csv"); 
   if (!fin){
     std::cout << "Unable to access db." << std::endl;
     std::cout << "Double-check file exists." << std::endl;
     exit(1); 
+  }
+
+  std::string line;
+  int record_number {-1}; 
+  while (!fin.eof()){
+    std::getline(fin, line); 
+
+    record_number++ ; 
+    std::cout << record_number << " - " 
+      << line << std::endl; 
   }
 }
 
@@ -30,5 +32,10 @@ void readFile(std::vector<Employee> employee_list){
 int main(){
 
   std::vector<Employee> employee_list; 
+
+  readFile(employee_list); 
+
+  //Employee John {12}; 
+  //std::cout << John.ID <<std::endl;
   return 0;
 }
